00ff89e6f2c82bda6f351d5d6a31e028ca1e23e2
Tim Jenkins <timothy_jenkins@comcast.com>
2017-12-16 16:34:30 -0500

    add route get one link, moves route functions into controller object

--- server.js
+++ server.js
@@ -24,34 +24,52 @@ mongoose.connection.once('open', () => {
   console.log('Successfully connected to the database');
 });
 
+const linkController = {
+  all: (req, res) => {
+    Link.find({}, (err, links) => {
+      if (err) return res.status(500).send('An error occurred while retrieving your links');
+
+      res.status(200).send(links);
+      return res;
+    });
+  },
+
+  one: (req, res) => {
+    Link.findOne({ _id: req.params.id }, (err, links) => {
+      if (err) return res.status(500).send('An error occurred while retrieving your links');
+
+      res.status(200).send(links);
+      return res;
+    });
+  },
+
+  new: (req, res) => {
+    const link = new Link();
+    link.name = req.body.name;
+    link.url = req.body.url;
+    link.dateCreated = new Date();
+    link.details = {
+      price: req.body.price,
+    };
+
+    link.save((err) => {
+      if (err) {
+        res.status(500).send(err);
+        res.send(err);
+      } else {
+        res.json({ message: `Link created ${link}` });
+      }
+    });
+  },
+};
 
 // Links Route
-app.get('/links', (req, res) => {
-  Link.find({}, (err, links) => {
-    if (err) return res.status(500).send('An error occurred while retrieving your links');
+app.get('/links', linkController.all);
 
-    res.status(200).send(links);
-    return res;
-  });
-});
+// Individual Link Route
+app.get('/links/:id', linkController.one);
 
-app.post('/links', (req, res) => {
-  const link = new Link();
-  link.name = req.body.name;
-  link.url = req.body.url;
-  link.dateCreated = new Date();
-  link.details = {
-    price: req.body.price,
-  };
-
-  // save the bear and check for errors
-  link.save((err) => {
-    if (err) {
-      res.send(err);
-    } else {
-      res.json({ message: `Link created ${link}` });
-    }
-  });
-});
+// Create New Link Route
+app.post('/links', linkController.new);
 
 app.listen(3000);
