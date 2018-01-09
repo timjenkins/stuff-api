6206b85360c6dcc35059f19948222d1de32e596a
Tim Jenkins <timothy_jenkins@comcast.com>
2018-01-09 15:41:46 -0500

    adds validation via express-validator, removes validation at mongoose schema level

--- api/controllers/linkController.js
+++ api/controllers/linkController.js
@@ -1,4 +1,5 @@
 const Link = require('../models/linkModel.js');
+const { validationResult } = require('express-validator/check');
 
 // Link Controller functions
 const linkController = {
@@ -12,30 +13,43 @@ const linkController = {
   },
 
   one: (req, res) => {
-    Link.findOne({ _id: req.params.id }, (err, link) => {
-      if (err) return res.status(500).send('An error occurred while retrieving your links');
-
-      res.status(200).send(link);
-      return res;
-    });
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(422).json({ errors: errors.mapped() });
+    } else {
+      Link.findOne({ _id: req.params.id }, (err, link) => {
+        if (err) {
+          res.status(500).send('An error occurred while retrieving your links');
+        } else {
+          res.status(200).send(link);
+        }
+      });
+    }
+    return res;
   },
 
   new: (req, res) => {
-    const link = new Link();
-    link.name = req.body.name;
-    link.url = req.body.url;
-    link.dateCreated = new Date();
-    link.details = {
-      price: req.body.price,
-    };
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(422).json({ errors: errors.mapped() });
+    } else {
+      const link = new Link();
+      link.title = req.body.title;
+      link.url = req.body.url;
+      link.dateCreated = new Date();
+      link.details = {
+        price: req.body.price,
+      };
 
-    link.save((err) => {
-      if (err) {
-        res.status(500).send(err);
-      } else {
-        res.status(200).send(link);
-      }
-    });
+      link.save((err) => {
+        if (err) {
+          res.status(500).send(err);
+        } else {
+          res.status(200).send(link);
+        }
+      });
+    }
+    return res;
   },
 };
 
--- api/controllers/loginController.js
+++ api/controllers/loginController.js
@@ -1,27 +1,33 @@
 const User = require('../models/userModel.js');
 const bcrypt = require('bcrypt');
 const { globalSalt } = require('../../db.config.js');
+const { validationResult } = require('express-validator/check');
 
 // Login Controller functions
 const loginController = {
   login: (req, res) => {
-    User.findOne({ email: req.body.email }, (err, foundUser) => {
-      if (err) {
-        res.status(500).send('User not found');
-      } else {
-        const enteredSaltedPassword = req.body.password + globalSalt;
-        bcrypt.compare(enteredSaltedPassword, foundUser.password, (hashError, compareRes) => {
-          if (hashError) {
-            res.status(500).send(hashError);
-          } else if (compareRes === true) {
-            res.status(400).send('Hooray! Password Matches!');
-          } else {
-            res.status(404).send(compareRes);
-          }
-          return res;
-        });
-      }
-    });
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(422).json({ errors: errors.mapped() });
+    } else {
+      User.findOne({ email: req.body.email }, (err, foundUser) => {
+        if (err) {
+          res.status(500).send('User not found');
+        } else {
+          const enteredSaltedPassword = req.body.password + globalSalt;
+          bcrypt.compare(enteredSaltedPassword, foundUser.password, (hashError, compareRes) => {
+            if (hashError) {
+              res.status(500).send(hashError);
+            } else if (compareRes === true) {
+              res.status(400).send('Hooray! Password Matches!');
+            } else {
+              res.status(404).send(compareRes);
+            }
+          });
+        }
+      });
+    }
+    return res;
   },
 };
 
