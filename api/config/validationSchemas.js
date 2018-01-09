e18c441012829853880433689582e3c5d0d22e4a
Tim Jenkins <timothy_jenkins@comcast.com>
2018-01-09 15:42:00 -0500

    adds files from prev. commit

---
+++ api/config/validationSchemas.js
@@ -0,0 +1,29 @@
+const { check } = require('express-validator/check');
+
+const validationSchemas = {
+  user: [
+    check('username').isAlphanumeric(),
+    check('firstName').isAlpha(),
+    check('lastName').isAlpha(),
+    check('email').isEmail(),
+    check('password').matches(/((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{8,64})/),
+  ],
+
+  login: [
+    check('email').isEmail(),
+    check('password').isAscii(),
+  ],
+
+  link: [
+    check('title').isAlpha(),
+    check('url').isURL(),
+    check('price').isCurrency({ allow_negatives: false }),
+  ],
+
+  linkId: [
+    check('id').isAlphanumeric(),
+  ],
+};
+
+
+module.exports = validationSchemas;
