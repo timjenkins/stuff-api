f2a3d8022a53861f89a1860e74cd9e4ed6164cf8
Tim Jenkins <timothy_jenkins@comcast.com>
2018-02-27 17:55:03 -0500

    refactored linkController to not need so many comments

--- api/controllers/linkController.js
+++ api/controllers/linkController.js
@@ -4,12 +4,9 @@ const { validationResult } = require('express-validator/check');
 // Link Controller functions
 const linkController = {
   all: (req, res) => {
-    // Find links from current user
-    Link.find({ user: req.user.id }, (err, links) => {
-      // Catch server errors
-      if (err) {
+    Link.find({ user: req.user.id }, (dbErr, links) => {
+      if (dbErr) {
         res.status(500).send('An error occurred while retrieving your links');
-      // Send links
       } else {
         res.status(200).send(links);
       }
@@ -18,19 +15,16 @@ const linkController = {
 
   one: (req, res) => {
     // Catch validation errors
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      res.status(422).json({ errors: errors.mapped() });
+    const validationErrors = validationResult(req);
+    if (!validationErrors.isEmpty()) {
+      res.status(422).json({ errors: validationErrors.mapped() });
     } else {
-      // Look up link
-      Link.findOne({ _id: req.params.id }, (err, link) => {
-        // Catch server errors
-        if (err) {
+      Link.findOne({ _id: req.params.id }, (dbErr, link) => {
+        if (dbErr) {
           res.status(500).send('An error occurred while retrieving your links');
         // Authorise user to view link
         } else if (req.user.id !== link.user) {
           res.status(401).send('Invalid permissions');
-        // Send link
         } else {
           res.status(200).send(link);
         }
@@ -40,14 +34,12 @@ const linkController = {
   },
 
   new: (req, res) => {
-    // Catch validation errors
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      res.status(422).json({ errors: errors.mapped() });
+    const validationErrors = validationResult(req);
+    if (!validationErrors.isEmpty()) {
+      res.status(422).json({ errors: validationErrors.mapped() });
     } else {
-      // Instantiate link
       const link = new Link();
-      // Set params
+
       link.title = req.body.title;
       link.url = req.body.url;
       link.user = req.user.id;
@@ -56,11 +48,9 @@ const linkController = {
         price: req.body.price,
       };
 
-      link.save((err) => {
-        // Catch server errors
-        if (err) {
-          res.status(500).send(err);
-        // Send success message with created link
+      link.save((dbErr) => {
+        if (dbErr) {
+          res.status(500).send(dbErr);
         } else {
           res.status(201).send(link);
         }
