0312b4120ecbc7167ed01ec61aac5fe0041b1e5a
Tim Jenkins <timothy_jenkins@comcast.com>
2018-03-21 18:03:45 -0400

    moves newrelic key into env, removes mongo url from env

--- config.js
+++ config.js
@@ -1,19 +1,20 @@
 require('dotenv').load();
 
 let envUrl = '';
+const prodEnvUrl = `mongodb://timjenkins:${process.env.PROD_MONGO_PWD}@stuffdb-shard-00-00-qg7z1.mongodb.net:27017,stuffdb-shard-00-01-qg7z1.mongodb.net:27017,stuffdb-shard-00-02-qg7z1.mongodb.net:27017/test?ssl=true&replicaSet=StuffDB-shard-0&authSource=admin`;
 
 switch (process.env.NODE_ENV) {
   case 'dev':
-    envUrl = process.env.PROD_MONGO_URL;
+    envUrl = prodEnvUrl;
     break;
   case 'prod':
-    envUrl = process.env.PROD_MONGO_URL;
+    envUrl = prodEnvUrl;
     break;
   case 'test':
     envUrl = 'mongodb://127.0.0.1:27017';
     break;
   default:
-    envUrl = process.env.PROD_MONGO_URL;
+    envUrl = prodEnvUrl;
 }
 
 
@@ -26,4 +27,3 @@ module.exports = {
     '/signup',
   ],
 };
-
